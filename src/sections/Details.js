bdb6e61353430e2aa7f44a9b2da0ec5d6e9eae91
Daniel Werthen <danielwerthen@gmail.com>
2019-05-20 21:07:34 +0200

    Update details

--- src/sections/Details.js
+++ src/sections/Details.js
@@ -54,23 +54,21 @@ export default function Details() {
           the end of the line.
         </p>
         <p>
-          After the party, we have arranged party buses bringing all of us home
-          (or to central Stockholm at least). These depart at 2.00 am so make
-          sure you keep your tickets (your invitations!). The party buses will
-          stop at XX and XX on or after 3.00 am. For earlier departure from the
-          party we recommend booking a taxi. The last bus is around ten to
-          midnight.
+          After the party, we have arranged party buses bringing all of us home (or to central Stockholm at least). These depart at 2.00 am so make sure you keep your tickets (your invitations!). The party buses will stop at the Central station and Mariatorget on or after 3.00 am. For earlier departure from the party we recommend booking a taxi. The last ordinary bus is around ten to midnight.
         </p>
         <h3>Accommodation</h3>
         <p>
-          For those of you travelling from far away we recommend HOTEL XXXX
-          which is located quite close to the bus drop off point at the end of
-          the night.
+          For those of you travelling from far away we recommend Hotell Rival or Hellstens Glashus which are located quite close to the last bus drop off point at the end of the night. But really any hotel in central Stockholm will do.
         </p>
         <h3>Dress code</h3>
         <p>
-          “Summer nice”/sommarfint. If you’re thinking “did they just make that
-          up to confuse me” - See the link below for inspiration LINK
+          “Summer nice”/sommarfint. If you’re thinking “did they just make that up to confuse me” - See the link below for inspiration.
+        </p>
+        <p>
+          <a href="https://pin.it/esn2tyatrngpvr">Pinterest</a>
+        </p>
+        <p>
+          If you still have questions, feel free to discuss with <a href="mailto:art.lindstrom@gmail.com">Art</a>.
         </p>
         <h3>Children</h3>
         <p>
@@ -79,10 +77,7 @@ export default function Details() {
         </p>
         <h3>Gifts</h3>
         <p>
-          Yes please! Ahem - I mean, the best possible gift is of course that
-          you celebrate this day with us. If, in addition to this lovely gift
-          you still want to give something more, Art has set up this lovely shop
-          in which you can contribute to our honeymoon(s). LINK
+          Yes please! Ahem - we mean, the best possible gift is of course that you celebrate this day with us. If, in addition to this lovely gift you still want to give something more, we would appreciate a contribution to our honeymoon(s). On the day, we will arrange a gift table with "gift cards". Each "gift card" will represent an experience or gift for us on our honeymoon, that you can treat us to. So the idea is you pick one, pay any amount you choose via Swish, and put the gift card in our gift bag (so no one else buys the same thing!) with your names on it so we know it's from you.
         </p>
         <h3>Mood managers</h3>
         <p>
